f529038047e9a295599825462f8318236c18ac32
Bopally <estelleb0707@gmail.com>
2025-03-18 20:46:01 +0100

    Fix Ember build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,15 @@ module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     fingerprint: {
       prepend: 'https://bopally.github.io/nasa-app/',
+      extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map'],
     },
+    autoImport: {
+      webpack: {
+        resolve: {
+          fullySpecified: false
+        }
+      }
+    }
   });
 
   return app.toTree();
