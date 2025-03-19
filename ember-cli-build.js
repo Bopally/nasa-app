9cae6163a619bcbb71e53ed6f624ab58ed6e5bf5
Bopally <estelleb0707@gmail.com>
2025-03-19 10:56:19 +0100

    Fix deployement from main on GitHub pages

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,19 +1,34 @@
 'use strict';
 
+// const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+// module.exports = function (defaults) {
+//   const app = new EmberApp(defaults, {
+//     fingerprint: {
+//       prepend: 'https://bopally.github.io/nasa-app/',
+//       extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map'],
+//     },
+//     autoImport: {
+//       webpack: {
+//         resolve: {
+//           fullySpecified: false
+//         }
+//       }
+//     }
+//   });
+
+//   return app.toTree();
+// };
+
+
+
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
+  let app = new EmberApp(defaults, {
     fingerprint: {
       prepend: 'https://bopally.github.io/nasa-app/',
       extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map'],
-    },
-    autoImport: {
-      webpack: {
-        resolve: {
-          fullySpecified: false
-        }
-      }
     }
   });
 
