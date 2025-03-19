50bde591d1c2e3f345c5afb5a283414b3dfd4c41
Bopally <estelleb0707@gmail.com>
2025-03-19 11:16:24 +0100

    Fix path from assets

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -27,7 +27,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      prepend: 'https://bopally.github.io/nasa-app/',
+      prepend: '/nasa-app/',
       extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map'],
     }
   });
