1c2cf7e8eb9bdfca50bc5c9a4da01ef62e18b366
Bopally <estelleb0707@gmail.com>
2025-03-27 18:15:03 +0100

    Fix loading state

--- app/controllers/missions.js
+++ app/controllers/missions.js
@@ -1,13 +1,31 @@
+import { tracked } from '@glimmer/tracking';
 import Controller from '@ember/controller';
 
 export default class MissionsController extends Controller {
-  isLoading = true; // L'état de chargement commence à true
+  @tracked isLoading = true;
+  @tracked missionsUrls = [];
 
-  init() {
-    super.init();
-    // Simuler un délai pour voir l'effet du loading
-    setTimeout(() => {
-      this.set('isLoading', false);
-    }, 1500);
+  constructor() {
+    super(...arguments);
+    this.fetchMissions();
+  }
+
+  async fetchMissions() {
+    const apiUrl = `/nasa-api/geode-py/ws/api/missions`;
+
+    try {
+      const response = await fetch(apiUrl, { method: 'GET', mode: 'cors' });
+      const data = await response.json();
+      this.missionsUrls = data.data.map((element) => {
+        const missionIdentifier = decodeURI(element.mission.split('/')[7]);
+        return missionIdentifier;
+      });
+    } catch (error) {
+      console.error('Error fetching missions:', error);
+      throw error;
+    } finally {
+      this.isLoading = false;
+    }
+    console.log(this.missionsUrls, this.isLoading);
   }
 }
